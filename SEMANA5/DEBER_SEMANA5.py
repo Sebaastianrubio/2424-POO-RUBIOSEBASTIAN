742b4ebb4fe07b64e557f1187430dcfed06b5970
Sebaastianrubio <as.rubiof@uea.edu.ec>
2024-06-29 20:11:58 -0500

    tarea semana5

--- SEMANA5/DEBER_SEMANA5.py
+++ SEMANA5/DEBER_SEMANA5.py
@@ -1,1 +1,6 @@
-print("hola mundo")
+#programa que calcule el area de circulo, intro
+pi=3.14 # Es la relación entre la longitud de una circunferencia y su diámetro
+print("Universidad Estatal Amazonica")
+radio=float(input("introduzca el radio en Cm:\n")) #Solicita ingresar el Valor de Radio
+area=pi*radio**2  # Formula para el Calculo del Area
+print("El area es:" ,area) # Despliega el Resultado
